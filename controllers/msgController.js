5db58371a4697bfe2725d83c8a9b751ff2c8f26d
Bleaze Perris <bperriscode@gmail.com>
2024-02-07 17:27:01 -0500

    created controller and views

--- controllers/msgController.js
+++ controllers/msgController.js
@@ -2,4 +2,25 @@ const Message = require('..models/message')
 
 const msg_main = (req, res) => {
     Message.find().sort({ createdAt: 1 })
+    .then((result) => {
+        res.render('index', {title: "All Messages", msgs: result})
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+}
+
+const msg_create_get = (req, res) => {
+    res.render('create', {title: 'Create new message'})
+}
+
+const msg_create_post = (req, res) => {
+    const msg = new Message(req.body)
+    msg.save()
+        .then(() => {
+            res.redirect('/index')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 }
